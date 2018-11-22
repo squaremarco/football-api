c027b187f9949c8192b2633f90cb318b7b46886f
squaremarco <sq@iamsquare.it>
2018-11-22 06:56:34 +0100

    Removed schemaConfig object Added seasons array to schema.

--- model/competition.js
+++ model/competition.js
@@ -1,33 +1,28 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const competitionSchemaConfig = {};
-
-const competitionSchema = new Schema(
-  {
+const competitionSchema = new Schema({
+  id: Number,
+  name: String,
+  code: String,
+  emblemUrl: String,
+  currentSeason: {
     id: Number,
-    name: String,
-    code: String,
-    emblemUrl: String,
-    currentSeason: {
+    startDate: Date,
+    endDate: Date,
+    currentMatchday: Number,
+    winner: { id: Number, name: String, shortName: String, tla: String, crestUrl: String }
+  },
+  seasons: [
+    {
       id: Number,
       startDate: Date,
       endDate: Date,
-      currentMatchday: Number,
-      winner: { id: Number, name: String, shortName: String, tla: String, crestUrl: String }
-    },
-    /*seasons: [
-      new Schema({
-        id: Number,
-        startDate: Date,
-        endDate: Date,
-        currentMatchday: Number
-      })
-    ],*/
-    lastUpdated: Date
-  },
-  competitionSchemaConfig
-);
+      currentMatchday: Number
+    }
+  ],
+  lastUpdated: Date
+});
 
 const competitionModel = mongoose.model('competition', competitionSchema);
 
