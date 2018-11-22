302945478a6694d507c51bd8d7393a9d99f5c62b
squaremarco <sq@iamsquare.it>
2018-11-22 06:58:28 +0100

    Modified scrapeApi

--- bin/scrapeApi.js
+++ bin/scrapeApi.js
@@ -1,14 +1,16 @@
-const { DB_HOST, DB_PORT, DB_NAME, API_BASE_URL, API_TOKEN } = require('../environment');
+const { DB_HOST, DB_PORT, DB_NAME, API_BASE_URL, API_TOKEN, API_PLAN } = process.env;
 
 const mongoose = require('mongoose');
 const axios = require('axios').create({
   baseURL: API_BASE_URL,
   headers: { 'X-Auth-Token': API_TOKEN }
 });
+
 const logger = require('../utils/logger');
 const mongooseExit = require('../utils/mongooseExit');
 
 const CompetitionModel = require('../model/competition').competitionModel;
+const MatchModel = require('../model/match').matchModel;
 
 mongoose.connect(
   `mongodb://${DB_HOST}:${DB_PORT}`,
@@ -20,24 +22,24 @@ mongoose.connection.on('error', err => {
 });
 
 process
-  .on('SIGTERM', () => mongooseExit('info', 'Gracefully closing connection with the database.', 0))
-  .on('SIGINT', () => mongooseExit('info', 'Gracefully closing connection with the database.', 0));
+  .on('SIGTERM', () => mongooseExit('info', 'SIGTERM received. Gracefully closing connection with the database.', 0))
+  .on('SIGINT', () => mongooseExit('info', 'SIGINT received. Gracefully closing connection with the database.', 0));
 
 mongoose.connection.once('connected', () => {
   logger.info(`Connected to ${mongoose.connection.db.databaseName}`);
 
-  //get competitions
-  axios.get('competitions?plan=TIER_ONE').then(res => {
-    let competitions = res.data.competitions;
+  //get competitions (2072 ENG - 2077 EU - 2081 FRA - 2114 ITA)
+  logger.info('Pulling Competitions from API');
+  axios
+    .get(`competitions?plan=${API_PLAN}&areas=2072,2081,2114`)
+    .then(res => {
+      const competitions = res.data.competitions;
 
-    //save them in the database
-    competitions.forEach(c => {
-      CompetitionModel.findOne({ id: c.id }, (err, doc) => {
-        if (err) mongooseExit('error', 'Error while querying the database.', -1);
-        if (!doc || new Date(c.lastUpdated) - new Date(doc.lastUpdated) !== 0) {
-          new CompetitionModel(c).save();
-        }
+      competitions.forEach(comp => {
+        axios.get(`competitions/${comp.id}`).then(res => {
+          const competition = res.data;
+        });
       });
-    });
-  });
+    })
+    .catch(error => mongooseExit('error', error, -1));
 });
