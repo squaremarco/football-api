9aa5ad17d9c238eb8a457cafff3bb065abe24bd0
squaremarco <sq@iamsquare.it>
2018-11-23 07:36:09 +0100

    Added axios-retry on error

--- bin/scrapeApi.js
+++ bin/scrapeApi.js
@@ -5,12 +5,28 @@ const axios = require('axios').create({
   baseURL: API_BASE_URL,
   headers: { 'X-Auth-Token': API_TOKEN }
 });
+const retry = require('axios-retry');
 
 const logger = require('../utils/logger');
 const mongooseExit = require('../utils/mongooseExit');
 
 const CompetitionModel = require('../model/competition').competitionModel;
 const MatchModel = require('../model/match').matchModel;
+
+retry(axios, {
+  retryCondition: () => error =>
+    (!error.response ||
+      error.response.status === 429 ||
+      (error.response.status >= 500 && error.response.status <= 599)) &&
+    Boolean(error.code) &&
+    error.code !== 'ECONNABORTED',
+  retryDelay: (_, err) => {
+    logger.error(`${err}, retrying...`);
+    return 60000;
+  },
+  shouldResetTimeout: true,
+  retries: 1
+});
 
 mongoose.connect(
   `mongodb://${DB_HOST}:${DB_PORT}`,
@@ -26,20 +42,38 @@ process
   .on('SIGINT', () => mongooseExit('info', 'SIGINT received. Gracefully closing connection with the database.', 0));
 
 mongoose.connection.once('connected', () => {
-  logger.info(`Connected to ${mongoose.connection.db.databaseName}`);
+  logger.info(`Connected to ${DB_HOST}:${DB_PORT}/${mongoose.connection.db.databaseName}`);
 
   //get competitions (2072 ENG - 2077 EU - 2081 FRA - 2114 ITA)
-  logger.info('Pulling Competitions from API');
+  logger.info('Pulling competitions from API');
   axios
     .get(`competitions?plan=${API_PLAN}&areas=2072,2081,2114`)
     .then(res => {
       const competitions = res.data.competitions;
 
       competitions.forEach(comp => {
-        axios.get(`competitions/${comp.id}`).then(res => {
-          const competition = res.data;
-        });
+        axios
+          .get(`competitions/${comp.id}`)
+          .then(res => {
+            const competition = res.data;
+
+            CompetitionModel.findOne({ id: competition.id }, (err, doc) => {
+              if (err) mongooseExit('error', err, -1);
+
+              if (!doc) {
+                logger.info(`Adding new competition ${competition.name} [id: ${competition.id}]`);
+                new CompetitionModel(competition).save();
+              } else if (doc && new Date(competition.lastUpdated) - doc.lastUpdated) {
+                logger.info(`Updating competition ${competition.name} [id: ${competition.id}]`);
+                doc.set(competition).save();
+              } else {
+                logger.warn(`Unchanged competition ${competition.name} [id: ${competition.id}], ignoring...`);
+              }
+            });
+          })
+          .catch(err => logger.error(err));
       });
     })
-    .catch(error => mongooseExit('error', error, -1));
+    .catch(err => logger.error(err));
+  CompetitionModel.find((err, docs) => {});
 });
